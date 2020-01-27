2dc7a24ff8bc80e4fcb00820f242da426ff979ef
百原實驗室 <baiyuan.lab@gmail.com>
2020-01-27 22:01:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo timedatectl set-timezone Asia/Taipei
 sudo apt update -y
 sudo apt dist-upgrade --assume-yes
 sudo apt autoremove -y
-sudo apt install htop iotop nmap mailutils -y
+sudo apt install htop iotop nmap -y
 
 #install Webinoly Optimized WebServer
 wget -qO weby qrok.es/wy && sudo bash weby 3
