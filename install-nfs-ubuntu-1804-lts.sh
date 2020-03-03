189ef5d31e0453b0e46a474690cc79e7b1c679c5
百原實驗室 <baiyuan.lab@gmail.com>
2020-03-03 12:46:41 +0800

    Update install-nfs-ubuntu-1804-lts.sh

--- install-nfs-ubuntu-1804-lts.sh
+++ install-nfs-ubuntu-1804-lts.sh
@@ -7,7 +7,7 @@ sudo apt install htop iotop nmap -y
 
 #Install NFS Server and Client on Ubuntu 18.04 LTS
 #參考資料 https://vitux.com/install-nfs-server-and-client-on-ubuntu
-sudo apt install nfs-kernel-server
+sudo apt install nfs-kernel-server -y
 sudo mkdir -p /mnt/baiyuan-nfs
 sudo chown nobody:nogroup /mnt/baiyuan-nfs
 sudo chmod 777 /mnt/baiyuan-nfs
