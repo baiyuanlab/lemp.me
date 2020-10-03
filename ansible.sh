804f0526d6f7521f1b6210cce7f1a42dc87a8987
百原實驗室 <baiyuan.lab@gmail.com>
2020-10-03 21:38:08 +0800

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo apt update
-sudo apt install software-properties-common
+sudo apt update -y
+sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
