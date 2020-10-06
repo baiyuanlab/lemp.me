a8bd8622ccae5b1f8d1bf4c35a15e5f80794d78e
百原實驗室 <baiyuan.lab@gmail.com>
2020-10-06 16:52:48 +0800

    Update ubuntu-up.sh

--- ubuntu-up.sh
+++ ubuntu-up.sh
@@ -4,3 +4,4 @@ sudo apt update -y
 sudo apt dist-upgrade --assume-yes
 sudo apt autoremove -y
 sudo apt install htop iotop nmap sysstat -y
+wget https://raw.githubusercontent.com/baiyuanlab/lemp.me/master/ubuntu-up.sh && bash ubuntu-up.sh
