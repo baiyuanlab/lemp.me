2cfa95439b3e4ef65ab2313d2cdb9a933e92353e
百原實驗室 <baiyuan.lab@gmail.com>
2020-10-23 01:18:07 +0800

    Delete ubuntu-up.sh

--- ubuntu-up.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo timedatectl set-timezone Asia/Taipei
-sudo apt update -y
-sudo apt dist-upgrade --assume-yes
-sudo apt autoremove -y
-sudo apt install htop iotop nmap sysstat -y
-wget https://raw.githubusercontent.com/baiyuanlab/lemp.me/master/ubuntu-up.sh && bash ubuntu-up.sh
